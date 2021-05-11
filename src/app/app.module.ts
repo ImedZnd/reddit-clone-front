19e0ff459e6679ded2de37ae32f0316a0f5a8db8
imed <imedzenned@yahoo.fr>
2021-05-11 04:13:50 +0200

    refactoring home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule , HTTP_INTERCEPTORS  } from '@angular/common/http';
 import { NgxWebstorageModule } from 'ngx-webstorage';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { TokenInterceptor } from './tokenInterceptor';
- 
+import { HomeComponent } from './home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { PostTileComponent } from './shared/post-tile/post-tile.component';
+import { VoteButtonComponent } from './shared/vote-button/vote-button.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { SubredditSideBarComponent } from './shared/subreddit-side-bar/subreddit-side-bar.component'
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
+    PostTileComponent,
+    VoteButtonComponent,
+    SideBarComponent,
+    SubredditSideBarComponent,
   ],
   imports: [
     BrowserModule,
@@ -24,12 +35,18 @@ import { TokenInterceptor } from './tokenInterceptor';
     HttpClientModule,
     NgxWebstorageModule.forRoot(),
     BrowserAnimationsModule,
-    ToastrModule
+    ToastrModule.forRoot({
+      positionClass :'toast-bottom-right'
+    }),
+    FontAwesomeModule
   ],
   providers: [
-    provide: HTTP_INTERCEPTORS,
-    useClass: TokenInterceptor,
-    multi: true
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+
   ],
   bootstrap: [AppComponent]
 })
--- src/app/tokenInterceptor.ts
+++ src/app/tokenInterceptor.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject, throwError } from 'rxjs';
 import { AuthService } from './auth/shared/auth.service';
-import { catchError, switchMap } from 'rxjs/operators';
+import { catchError, switchMap, take, filter } from 'rxjs/operators';
 import { LoginResponsePayload } from './model/login-response.payload';
+
 
 @Injectable({
     providedIn: 'root'
@@ -11,24 +12,34 @@ import { LoginResponsePayload } from './model/login-response.payload';
 export class TokenInterceptor implements HttpInterceptor {
 
     isTokenRefreshing = false;
-    refreshTokenSubject: BehaviorSubject = new BehaviorSubject(null);
-    constructor(public authService: AuthService,
-                private httpRequest:HttpRequest) { }
+    refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject(null);
 
-    intercept(req: HttpRequest<any>, next:HttpHandler): Observable<HttpEvent<any>> {
-            const jwtToken = this.authService.getJwtToken();
-        if (jwtToken) {
-            this.addToken(req, jwtToken);
+    constructor(public authService: AuthService) { }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler):
+        Observable<HttpEvent<any>> {
+
+        if (req.url.indexOf('refresh') !== -1 || req.url.indexOf('login') !== -1) {
+            return next.handle(req);
         }
-        return next.handle(req).pipe(catchError(error => {
-            if(error instanceof HttpErrorResponse && error.status ===403){
-                return this.handleAuthErrors(req,next);
-            }else{
-                return throwError(error);
-            }
-        }));
+        const jwtToken = this.authService.getJwtToken();
+
+        if (jwtToken) {
+            return next.handle(this.addToken(req, jwtToken)).pipe(catchError(error => {
+                if (error instanceof HttpErrorResponse
+                    && error.status === 403) {
+                    return this.handleAuthErrors(req, next);
+                } else {
+                    return throwError(error);
+                }
+            }));
+        }
+        return next.handle(req);
+
     }
-    private handleAuthErrors(req: HttpRequest, next: HttpHandler) {
+
+    private handleAuthErrors(req: HttpRequest<any>, next: HttpHandler)
+        : Observable<HttpEvent<any>> {
         if (!this.isTokenRefreshing) {
             this.isTokenRefreshing = true;
             this.refreshTokenSubject.next(null);
@@ -36,16 +47,25 @@ export class TokenInterceptor implements HttpInterceptor {
             return this.authService.refreshToken().pipe(
                 switchMap((refreshTokenResponse: LoginResponsePayload) => {
                     this.isTokenRefreshing = false;
-                    this.refreshTokenSubject.next(refreshTokenResponse.authenticationToken);
-                    return next.handle(this.addToken(req, refreshTokenResponse.authenticationToken));
+                    this.refreshTokenSubject
+                        .next(refreshTokenResponse.authenticationToken);
+                    return next.handle(this.addToken(req,
+                        refreshTokenResponse.authenticationToken));
                 })
             )
+        } else {
+            return this.refreshTokenSubject.pipe(
+                filter(result => result !== null),
+                take(1),
+                switchMap((res) => {
+                    return next.handle(this.addToken(req,
+                        this.authService.getJwtToken()))
+                })
+            );
         }
     }
 
-
-
-    private addToken(req: HttpRequest<any>, jwtToken: string) {
+    addToken(req: HttpRequest<any>, jwtToken: any) {
         return req.clone({
             headers: req.headers.set('Authorization',
                 'Bearer ' + jwtToken)
